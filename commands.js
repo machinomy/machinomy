c95bdc028139f708ff72749e7263b11a562399cc
Sergey Ukustov <sergey@ukstv.me>
2016-11-17 01:31:44 +0300

    Basic pry <uri> subcommand for getting info on a resource cost

--- commands.js
+++ commands.js
@@ -2,7 +2,8 @@
 
 var commander = require("commander"),
     machinomy = require("./index"),
-    buy = require("./commands/buy");
+    buy = require("./commands/buy"),
+    pry = require("./commands/pry");
 
 var main = function (args) {
     var version = machinomy.NAME + " v" + machinomy.VERSION;
@@ -11,6 +12,10 @@ var main = function (args) {
     parser.command("buy <uri>")
         .description("buy a resource at <uri>")
         .action(buy);
+
+    parser.command("pry <uri>")
+        .description("see cost of a resource at <uri>")
+        .action(pry);
 
     parser.parse(args);
 };
