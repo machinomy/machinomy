33282785bbc69a664d67fc61b9a27a015a82f22e
Sergey Ukustov <sergey@ukstv.me>
2017-01-07 14:03:52 +0300

    No color in machinomy setup prompt

--- commands/setup.js
+++ commands/setup.js
@@ -19,13 +19,14 @@ var setup = function (command) {
         configuration = {};
     }
 
-    console.log(configuration);
-
+    prompt.message = null;
+    prompt.colors = false;
     prompt.start();
     console.log("Please, for a command line client insert you Ethereum account address, and optionally a password");
     console.log("For " + namespace);
     prompt.get(["account", "password"], function (err, result) {
         configuration[namespace] = result;
+        console.log("");
         console.log("Full configuration:");
         console.log(configuration);
         var configurationString = JSON.stringify(configuration, null, 4);
