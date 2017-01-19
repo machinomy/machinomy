dc8a492d1255d87e1e96c53e2960ec4e6e610261
Sergey Ukustov <sergey@ukstv.me>
2017-01-19 03:43:48 +0300

    Bump version to 0.1.5

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var buy = function (uri, account, password, _callback) {
 
 module.exports = {
     NAME: "machinomy",
-    VERSION: "0.1.4",
+    VERSION: "0.1.5",
     Paywall: middleware.Paywall,
     Transport: transport.Transport,
     Client: transport.Client,
