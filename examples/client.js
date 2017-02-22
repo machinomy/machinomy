52bd40deb2c2ee2edd3e6ce62fa540ece298e230
Sergey Ukustov <sergey@ukstv.me>
2017-02-22 05:26:57 +0300

    examples use StandardJS style guide

--- examples/client.js
+++ examples/client.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict'
 
-var machinomy = require("../index");
+var machinomy = require('../index')
 
-var uri = process.argv.pop();
+var uri = process.argv.pop()
 
-var settings = machinomy.configuration.sender();
+var settings = machinomy.configuration.sender()
 machinomy.buy(uri, settings.account, settings.password, function (err, contents) {
-    if (err) throw err;
-    console.log(contents);
-});
+  if (err) throw err
+  console.log(contents)
+})
--- examples/server.js
+++ examples/server.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict'
 
-var express = require("express");
-var bodyParser = require('body-parser');
-var machinomy = require("./../index");
+var express = require('express')
+var bodyParser = require('body-parser')
+var machinomy = require('./../index')
 
-var settings = machinomy.configuration.receiver();
-machinomy.web3.personal.unlockAccount(settings.account, settings.password, 1000);
+var settings = machinomy.configuration.receiver()
+machinomy.web3.personal.unlockAccount(settings.account, settings.password, 1000)
 
-var paywall = new machinomy.Paywall(settings.account, 'http://localhost:3000');
+var paywall = new machinomy.Paywall(settings.account, 'http://localhost:3000')
 
-var app = express();
-app.use(bodyParser.json());
-app.use(paywall.middleware());
+var app = express()
+app.use(bodyParser.json())
+app.use(paywall.middleware())
 
-app.get("/hello", paywall.guard(1000, function (req, res) {
-    res.write("Have just received 1000 wei.\n");
-    res.end("Hello, meat world!");
-}));
+app.get('/hello', paywall.guard(1000, function (req, res) {
+  res.write('Have just received 1000 wei.\n')
+  res.end('Hello, meat world!')
+}))
 
 app.listen(3000, function(_) {
-    console.log("Waiting at http://localhost:3000/hello");
-});
+  console.log('Waiting at http://localhost:3000/hello')
+})
