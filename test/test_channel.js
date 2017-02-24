c10dbca6d81c54ca8c8a5b728880459e4f8e2eea
Sergey Ukustov <sergey@ukstv.me>
2017-02-24 23:06:29 +0300

    Add channel.ChannelId#toString

--- test/test_channel.js
+++ test/test_channel.js
@@ -10,17 +10,18 @@ const channel = require('../lib/channel')
 const describe = mocha.describe
 const it = mocha.it
 
+const HEX_ADDRESS = 'eb61859a9d74f95bda8a6f9d3efcfe6478e49151'
+
 describe('channel', () => {
   describe('.id', () => {
-    const hex = 'eb61859a9d74f95bda8a6f9d3efcfe6478e49151'
-    const buffer = Buffer.from(hex, 'hex')
+    const buffer = Buffer.from(HEX_ADDRESS, 'hex')
     const expected = new channel.ChannelId(buffer)
     it('builds ChannelId from non-prefixed hex', () => {
-      let channelId = channel.id(hex)
+      let channelId = channel.id(HEX_ADDRESS)
       assert.deepEqual(channelId, expected)
     })
     it('builds ChannelId from prefixed hex', () => {
-      let channelId = channel.id('0x' + hex)
+      let channelId = channel.id('0x' + HEX_ADDRESS)
       assert.deepEqual(channelId, expected)
     })
     it('builds ChannelId from Buffer', () => {
@@ -30,6 +31,16 @@ describe('channel', () => {
     it('builds ChannelId from ChannelId', () => {
       let channelId = channel.id(expected)
       assert.deepEqual(channelId, expected)
+    })
+  })
+
+  describe('ChannelId', () => {
+    describe('#toString', () => {
+      it('returns prefixed hex', () => {
+        let channelId = channel.id(HEX_ADDRESS)
+        let actual = channelId.toString()
+        assert.equal(actual, '0x' + HEX_ADDRESS)
+      })
     })
   })
 })
