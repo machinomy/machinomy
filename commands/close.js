cdb4a84f7eb5b6b30ced263783f49e8cca6298e1
Sergey Ukustov <sergey@ukstv.me>
2017-02-25 18:38:58 +0300

    Groom close command towards Standard JS

--- commands/close.js
+++ commands/close.js
@@ -1,89 +1,91 @@
-"use strict";
+'use strict'
 
-var machinomy = require("../index"),
-    web3 = machinomy.web3;
+const machinomy = require('../index')
 
-var claim = function (storage, contract, paymentChannel) {
-    var channelId = paymentChannel.channelId;
-    storage.lastPaymentDoc(channelId, function (error, paymentDoc) {
-        if (error) throw error;
-        var canClaim = contract.canClaim(channelId, paymentDoc.value, Number(paymentDoc.v), paymentDoc.r, paymentDoc.s);
-        if (canClaim) {
-            contract.claim(paymentChannel.receiver, paymentChannel.channelId, paymentDoc.value, Number(paymentDoc.v), paymentDoc.r, paymentDoc.s, function (error, value) {
-                if (error) throw error;
-                console.log("Claimed " + value + " out of " + paymentChannel.value + " from channel " + channelId);
-            });
-        } else {
-            console.log("Can not claim " + paymentDoc.value + " from channel " + channelId);
-        }
-    });
-};
+const web3 = machinomy.web3
+
+const claim = function (storage, contract, paymentChannel) {
+  let channelId = paymentChannel.channelId
+  storage.lastPaymentDoc(channelId, function (error, paymentDoc) {
+    if (error) throw error
+    var canClaim = contract.canClaim(channelId, paymentDoc.value, Number(paymentDoc.v), paymentDoc.r, paymentDoc.s)
+    if (canClaim) {
+      contract.claim(paymentChannel.receiver, paymentChannel.channelId, paymentDoc.value, Number(paymentDoc.v), paymentDoc.r, paymentDoc.s, function (error, value) {
+        if (error) throw error
+        console.log('Claimed ' + value + ' out of ' + paymentChannel.value + ' from channel ' + channelId)
+      })
+    } else {
+      console.log('Can not claim ' + paymentDoc.value + ' from channel ' + channelId)
+    }
+  })
+}
 
 var startSettle = function (settings, contract, paymentChannel) {
-    var canStartSettle = contract.canStartSettle(settings.account, paymentChannel.channelId);
-    if (canStartSettle) {
-        contract.startSettle(settings.account, paymentChannel.channelId, paymentChannel.spent, function (error) {
-            if (error) throw error;
-            console.log("Start settling channel " + paymentChannel.channelId);
-        });
-    } else {
-        console.log("Can not start settling channel " + paymentChannel.channelId);
-    }
-};
+  var canStartSettle = contract.canStartSettle(settings.account, paymentChannel.channelId)
+  if (canStartSettle) {
+    contract.startSettle(settings.account, paymentChannel.channelId, paymentChannel.spent, function (error) {
+      if (error) throw error
+      console.log('Start settling channel ' + paymentChannel.channelId)
+    })
+  } else {
+    console.log('Can not start settling channel ' + paymentChannel.channelId)
+  }
+}
 
 var finishSettle = function (settings, contract, paymentChannel) {
-    if (contract.canFinishSettle(settings.account, paymentChannel.channelId)) {
-        contract.finishSettle(settings.account, paymentChannel.channelId, function (error, payment) {
-            console.log("Settled to pay " + payment + " to " + paymentChannel.receiver);
-        });
-    } else {
-        var until = contract.getUntil(paymentChannel.channelId);
-        console.log("Can not finish settle until " + until);
-    }
-};
+  if (contract.canFinishSettle(settings.account, paymentChannel.channelId)) {
+    contract.finishSettle(settings.account, paymentChannel.channelId, function (error, payment) {
+      if (error) throw error
+      console.log('Settled to pay ' + payment + ' to ' + paymentChannel.receiver)
+    })
+  } else {
+    var until = contract.getUntil(paymentChannel.channelId)
+    console.log('Can not finish settle until ' + until)
+  }
+}
 
 /**
  * @param {String} channelId
  * @param {Object} options
  */
 var close = function (channelId, options) {
-    var namespace = options.namespace || "sender";
+  var namespace = options.namespace || 'sender'
 
-    var settings = machinomy.configuration[namespace].call();
-    var password = options.parent.password || settings.password;
+  var settings = machinomy.configuration[namespace].call()
+  var password = options.parent.password || settings.password
 
-    web3.personal.unlockAccount(settings.account, password, 1000);
+  web3.personal.unlockAccount(settings.account, password, 1000)
 
-    var storage = new machinomy.Storage(settings.databaseFile, namespace);
-    var contract = machinomy.contract;
+  var storage = new machinomy.Storage(settings.databaseFile, namespace)
+  var contract = machinomy.contract
 
-    storage.channelByChannelId(channelId, function (err, paymentChannel) {
-        if (err) throw err;
-        var state = contract.getState(channelId);
-        switch(state) {
-            case 0: // open
-                console.log("Channel " + channelId + " is open");
-                if (settings.account == paymentChannel.sender) {
-                    startSettle(settings, contract, paymentChannel);
-                } else if (settings.account == paymentChannel.receiver) {
-                    claim(storage, contract, paymentChannel);
-                }
-                break;
-            case 1: // settling
-                console.log("Channel " + channelId + " is settling");
-                if (settings.account == paymentChannel.sender) {
-                    finishSettle(settings, contract, paymentChannel);
-                } else if (settings.account == paymentChannel.receiver) {
-                    claim(storage, contract, paymentChannel);
-                }
-                break;
-            case 2: // settled, nothing to do
-                console.log("Channel " + channelId + " is settled");
-                break;
-            default:
-                throw "Unsupported channel state: " + state;
+  storage.channelByChannelId(channelId, function (err, paymentChannel) {
+    if (err) throw err
+    var state = contract.getState(channelId)
+    switch (state) {
+      case 0: // open
+        console.log('Channel ' + channelId + ' is open')
+        if (settings.account === paymentChannel.sender) {
+          startSettle(settings, contract, paymentChannel)
+        } else if (settings.account === paymentChannel.receiver) {
+          claim(storage, contract, paymentChannel)
         }
-    });
-};
+        break
+      case 1: // settling
+        console.log('Channel ' + channelId + ' is settling')
+        if (settings.account === paymentChannel.sender) {
+          finishSettle(settings, contract, paymentChannel)
+        } else if (settings.account === paymentChannel.receiver) {
+          claim(storage, contract, paymentChannel)
+        }
+        break
+      case 2: // settled, nothing to do
+        console.log('Channel ' + channelId + ' is settled')
+        break
+      default:
+        throw new Error('Unsupported channel state: ' + state)
+    }
+  })
+}
 
-module.exports = close;
+module.exports = close
