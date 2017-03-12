cbf874f3b83ee88245dca5d53fb3c759c9c9997d
Sergey Ukustov <sergey@ukstv.me>
2017-03-12 16:32:01 +0300

    Pry command uses new web3

--- commands/pry.js
+++ commands/pry.js
@@ -6,7 +6,9 @@ const pry = (uri) => {
   let settings = machinomy.configuration.sender()
   let transport = machinomy.transport.build()
   let storage = new machinomy.Storage(settings.databaseFile, 'sender')
-  let client = machinomy.sender.build(settings.account, machinomy.contract, transport, storage)
+  let web3 = machinomy.configuration.web3()
+  let contract = machinomy.contract(web3)
+  let client = machinomy.sender.build(settings.account, contract, transport, storage)
   client.pry(uri).then(paymentRequired => {
     console.log(paymentRequired)
   }).catch(error => {
