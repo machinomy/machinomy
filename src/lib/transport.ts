74ec957da331f77c0f00c52159f0c43b09784ca5
Sergey Ukustov <sergey@ukstv.me>
2017-07-10 04:27:27 +0300

    transport.ts

---
+++ src/lib/transport.ts
@@ -0,0 +1,123 @@
+import Promise = require('bluebird')
+import { Log } from 'ng2-logger'
+import _ = require('lodash')
+import { RequestResponse, RequiredUriUrl, CoreOptions } from 'request'
+import { Payment } from './channel'
+const request: (opts: RequiredUriUrl & CoreOptions) => Promise<RequestResponse> = Promise.promisify(require('request'))
+
+const log = Log.create('transport')
+
+// noinspection MagicNumberJS
+export const STATUS_CODES = {
+  PAYMENT_REQUIRED: 402
+}
+
+/**
+ * Parse response headers and return the token.
+ *
+ * @param {object} response
+ * @return {string}
+ */
+const extractPaywallToken = (response: RequestResponse) => {
+  let token = response.headers['paywall-token']
+  if (token) {
+    log.info('Got token from the server')
+    return token
+  } else {
+    throw new Error('Can not find a token in the response')
+  }
+}
+
+export interface GetWithTokenCallbacks {
+  onWillLoad?: Function,
+  onDidLoad?: Function
+}
+
+export interface RequestTokenCallbacks {
+  onWillSendPayment?: Function
+  onDidSendPayment?: Function
+}
+
+export class Transport {
+    /**
+     * Request URI sending a paywall token.
+     * @return {Promise<object>}
+     */
+  getWithToken (uri: string, token: string, opts: GetWithTokenCallbacks = {}): Promise<object> {
+    let headers = {
+      'authorization': 'Paywall ' + token
+    }
+    log.info(`Getting ${uri} using access token ${token}`)
+    if (_.isFunction(opts.onWillLoad)) {
+      opts.onWillLoad()
+    }
+    return this.get(uri, headers).then(result => {
+      if (_.isFunction(opts.onDidLoad)) {
+        opts.onDidLoad()
+      }
+      return result
+    })
+  }
+
+  get (uri: string, headers?: object): Promise<object> {
+    let options = {
+      method: 'GET',
+      uri: uri,
+      headers: headers
+    }
+    log.info(`Getting ${uri} using headers and options`, headers, options)
+    return request(options)
+  }
+
+    /**
+     * Request token from the server's gateway
+     * @param {string} uri - Full url to the gateway.
+     * @param {Payment} payment
+     * @param {{uri: string, headers: object, onWillPreflight: function, onDidPreflight: function, onWillOpenChannel: function, onDidOpenChannel: function, onWillSendPayment: function, onDidSendPayment: function, onWillLoad: function, onDidLoad: function}} opts
+     * @return {Promise<string>}
+     */
+  requestToken (uri: string, payment: Payment, opts: RequestTokenCallbacks = {}) {
+    let options = {
+      method: 'POST',
+      uri: uri,
+      json: true,
+      body: payment
+    }
+    log.info('Getting request token in exchange for payment', payment)
+    if (_.isFunction(opts.onWillSendPayment)) {
+      opts.onWillSendPayment()
+    }
+    return request(options).then(extractPaywallToken).then(result => {
+      if (_.isFunction(opts.onDidSendPayment)) {
+        opts.onDidSendPayment()
+      }
+      return result
+    })
+  }
+}
+
+export class PaymentRequired {
+  receiver: string
+  price: number
+  gateway: string
+
+  constructor (receiver: string, price: number, gateway: string) {
+    this.receiver = receiver
+    this.price = price
+    this.gateway = gateway
+  }
+
+  static parse = function (headers: any): PaymentRequired {
+    let receiver = headers['paywall-address']
+    let price = Number(headers['paywall-price'])
+    let gateway = headers['paywall-gateway']
+    return new PaymentRequired(receiver, price, gateway)
+  }
+}
+
+/**
+ * Build Transport instance.
+ */
+export const build = (): Transport => {
+  return new Transport()
+}
