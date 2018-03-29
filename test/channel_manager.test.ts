4938ac16993b2e7b0ae063413be90b355c30ea1d
Andrei Riaskov <ryaskov.work@gmail.com>
2018-03-29 16:48:13 +0300

    Fixes for tslint. Part 3.

--- test/channel_manager.test.ts
+++ test/channel_manager.test.ts
@@ -366,9 +366,6 @@ describe('ChannelManagerImpl', () => {
   })
 
   describe('acceptPayment', () => {
-    // tslint:disable-next-line:no-unused-variable
-    let channel: PaymentChannel
-
     let payment: Payment
 
     beforeEach(() => {
@@ -391,7 +388,6 @@ describe('ChannelManagerImpl', () => {
         token: ''
       } as Payment
 
-      channel = new PaymentChannel('0xcafe', '0xbeef', id, new BigNumber.BigNumber(10), new BigNumber.BigNumber(2), 0, undefined)
       deployed.channels = sinon.stub().resolves(['0', '0',
         new BigNumber.BigNumber(8), new BigNumber.BigNumber(0), new BigNumber.BigNumber(0)])
     })
