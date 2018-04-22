53c39c14e4ab0326f0f10b741a8e8f5604cef406
ukstv <sergey.ukustov@machinomy.com>
2018-04-23 00:54:53 +0200

    Add PaymentValidation

---
+++ lib/PaymentManager.ts
@@ -0,0 +1,44 @@
+import * as BigNumber from 'bignumber.js'
+import ChainManager from './chain_manager'
+import { PaymentChannel } from './payment_channel'
+import Payment from './payment'
+import ChannelContract from './channel_contract'
+import { MachinomyOptions } from '../MachinomyOptions'
+import PaymentValidation from './PaymentValidation'
+
+export default class PaymentManager {
+  private chainManager: ChainManager
+
+  private channelContract: ChannelContract
+
+  private options: MachinomyOptions
+
+  constructor (chainManager: ChainManager, channelContract: ChannelContract, options: MachinomyOptions) {
+    this.chainManager = chainManager
+    this.channelContract = channelContract
+    this.options = options
+  }
+
+  async buildPaymentForChannel (channel: PaymentChannel, price: BigNumber.BigNumber, totalValue: BigNumber.BigNumber, meta: string): Promise<Payment> {
+    const digest = await this.channelContract.paymentDigest(channel.channelId, totalValue)
+    const signature = await this.chainManager.sign(channel.sender, digest)
+
+    return new Payment({
+      channelId: channel.channelId,
+      sender: channel.sender,
+      receiver: channel.receiver,
+      price,
+      value: totalValue,
+      channelValue: channel.value,
+      signature,
+      meta,
+      contractAddress: channel.contractAddress,
+      token: undefined
+    })
+  }
+
+  async isValid (payment: Payment, paymentChannel: PaymentChannel): Promise<boolean> {
+    let validation = new PaymentValidation(this.channelContract, payment, paymentChannel, this.options)
+    return validation.isValid()
+  }
+}
---
+++ lib/PaymentValidation.ts
@@ -0,0 +1,106 @@
+import * as BigNumber from 'bignumber.js'
+
+import { PaymentChannel } from './payment_channel'
+import Payment from './payment'
+import log from './util/log'
+import ChannelContract from './channel_contract'
+import { DEFAULT_SETTLEMENT_PERIOD } from './channel_manager'
+import { MachinomyOptions } from '../MachinomyOptions'
+
+const LOG = log('PaymentValidation')
+
+function error (message: string, ...args: Array<any>) {
+  LOG(`Payment is invalid: ${message}`, args)
+}
+
+export default class PaymentValidation {
+  private readonly payment: Payment
+  private readonly paymentChannel: PaymentChannel
+  private readonly channelContract: ChannelContract
+  private readonly options: MachinomyOptions
+
+  constructor (channelContract: ChannelContract, payment: Payment, paymentChannel: PaymentChannel, options: MachinomyOptions) {
+    this.payment = payment
+    this.paymentChannel = paymentChannel
+    this.channelContract = channelContract
+    this.options = options
+  }
+
+  async isValid (): Promise<boolean> {
+    return this.isValidIncrement() &&
+      this.isValidChannelValue() &&
+      this.isValidChannelId() &&
+      this.isValidPaymentValue() &&
+      this.isValidSender() &&
+      this.isPositive() &&
+      this.canClaim() &&
+      this.isAboveMinSettlementPeriod()
+  }
+
+  private async isValidIncrement (): Promise<boolean> {
+    const isValidIncrement = this.paymentChannel.spent.plus(this.payment.price).equals(this.payment.value)
+    if (!isValidIncrement) {
+      error(`Price increment is too large. Payment channel already spent: ${this.paymentChannel.spent}, payment: %o`, this.payment)
+    }
+    return isValidIncrement
+  }
+
+  private async isValidChannelValue (): Promise<boolean> {
+    const isValidChannelValue = this.paymentChannel.value.equals(this.payment.channelValue)
+    if (!isValidChannelValue) {
+      error(`Payment value does not match payment channel value. payment channel value: ${this.paymentChannel.value}, payment: %o`, this.payment)
+    }
+    return isValidChannelValue
+  }
+
+  private async isValidChannelId (): Promise<boolean> {
+    const isValidChannelId = this.paymentChannel.channelId === this.payment.channelId
+    if (!isValidChannelId) {
+      error(`Channel Id does not match. expected: ${this.paymentChannel.channelId}, payment: %o`, this.payment)
+    }
+    return isValidChannelId
+  }
+
+  private async isValidPaymentValue (): Promise<boolean> {
+    const isValidPaymentValue = this.paymentChannel.value.lessThanOrEqualTo(this.payment.channelValue)
+    if (!isValidPaymentValue) {
+      error(`Payment value exceeds the channel value. Channel value: ${this.paymentChannel.value}, payment: %o`, this.payment)
+    }
+    return isValidPaymentValue
+  }
+
+  private async isValidSender (): Promise<boolean> {
+    const isValidSender = this.paymentChannel.sender === this.payment.sender
+    if (!isValidSender) {
+      error(`Sender does not match. Channel sender: ${this.paymentChannel.sender}, payment: %o`, this.payment)
+    }
+    return isValidSender
+  }
+
+  private async isPositive (): Promise<boolean> {
+    const isPositive = this.payment.value.greaterThanOrEqualTo(0) && this.payment.price.greaterThanOrEqualTo(0)
+    if (!isPositive) {
+      error(`payment is invalid because the price or value is negative. payment: %o`, this.payment)
+    }
+    return isPositive
+  }
+
+  private async canClaim (): Promise<boolean> {
+    let p = this.payment
+    const canClaim = await this.channelContract.canClaim(p.channelId, p.value, p.receiver, p.signature)
+    if (!canClaim) {
+      error(`Channel contract cannot accept the claim. Payment: %o`, p)
+    }
+    return canClaim
+  }
+
+  private async isAboveMinSettlementPeriod (): Promise<boolean> {
+    const settlementPeriod = await this.channelContract.getSettlementPeriod(this.payment.channelId)
+    const minSettlementPeriod = new BigNumber.BigNumber(this.options.minimumSettlementPeriod || DEFAULT_SETTLEMENT_PERIOD)
+    const isAboveMinSettlementPeriod = minSettlementPeriod.lessThanOrEqualTo(settlementPeriod)
+    if (!isAboveMinSettlementPeriod) {
+      error(`Settlement period is too short. settlement period: ${settlementPeriod}, minimum: ${minSettlementPeriod}. payment: %o`, this.payment)
+    }
+    return isAboveMinSettlementPeriod
+  }
+}
