0be43c3d8619883042291911c695accba0d04362
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-05 18:54:57 +0300

    Fix playground for work with hub instead of Machinomy calls.

--- packages/playground/src/Paywall.ts
+++ packages/playground/src/Paywall.ts
@@ -1,15 +1,20 @@
-import * as BigNumber from 'bignumber.js'
 import * as debug from 'debug'
+import * as dotenv from 'dotenv'
 import * as express from 'express'
-import * as urljoin from 'url-join'
-import Machinomy from 'machinomy'
 import { URL } from 'url'
+import * as urljoin from 'url-join'
+import fetcher from '../../machinomy/lib/util/fetcher'
+import * as BigNumber from 'bignumber.js'
 
 const log = debug('paywall')
 
 const HEADER_NAME = 'authorization'
 const TOKEN_NAME = 'paywall'
-const PREFIX = '/.well-known/machinomy'
+const PREFIX = '/v1'
+
+dotenv.config()
+
+const GATEWAY_URL = process.env.GATEWAY_URL
 
 function acceptUrl (base: URL) {
   return urljoin(base.toString(), PREFIX, 'accept')
@@ -28,14 +33,16 @@ function paywallHeaders (receiverAccount: string, gatewayUri: string, price: Big
   return headers
 }
 
-function parseToken (req: express.Request, callback: (error: string | null, token?: string) => void) {
+function parseToken (req: express.Request, callback: (error: string | null, token?: string, meta?: string, price?: BigNumber.BigNumber) => void) {
   let content = req.get(HEADER_NAME)
   if (content) {
     let authorization = content.split(' ')
     let type = authorization[0].toLowerCase()
     let token = authorization[1]
+    let meta = authorization[2]
+    let price = new BigNumber.BigNumber(authorization[3])
     if (type === TOKEN_NAME) {
-      callback(null, token)
+      callback(null, token, meta, price)
     } else {
       callback(`Invalid ${HEADER_NAME} token name present. Expected ${TOKEN_NAME}, got ${type}`)
     }
@@ -47,10 +54,8 @@ function parseToken (req: express.Request, callback: (error: string | null, toke
 export default class Paywall {
   receiverAccount: string
   base: URL
-  machinomy: Machinomy
 
-  constructor (machinomy: Machinomy, receiverAccount: string, base: URL) {
-    this.machinomy = machinomy
+  constructor (receiverAccount: string, base: URL) {
     this.receiverAccount = receiverAccount
     this.base = base
   }
@@ -64,30 +69,33 @@ export default class Paywall {
   }
 
   guard (price: BigNumber.BigNumber, callback: express.RequestHandler): express.RequestHandler {
-    let _guard = async (fixedPrice: BigNumber.BigNumber, req: express.Request, res: express.Response, next: express.NextFunction, error: any, token?: string) => {
+    let _guard = async (fixedPrice: BigNumber.BigNumber, req: express.Request, res: express.Response, next: express.NextFunction, error: any, token?: string, meta?: string) => {
       if (error || !token) {
         log(error)
         this.paymentRequired(fixedPrice, req, res)
       } else {
-        this.machinomy.acceptToken({ token }).then(canAccept => {
-          let isOk = canAccept.status
-          if (isOk) {
-            log('Got valid paywall token')
-            callback(req, res, next)
-          } else {
-            log('Got invalid paywall token')
-            this.paymentInvalid(fixedPrice, req, res)
-          }
-        }).catch(error => {
-          next(error)
+        const response = await fetcher.fetch(`${GATEWAY_URL}${PREFIX}/verify?token=${token}&meta=${meta}&price=${price}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include'
         })
+
+        if (response.status >= 200 && response.status < 300) {
+          log('Got valid paywall token')
+          callback(req, res, next)
+        } else {
+          log('Got invalid paywall token')
+          this.paymentInvalid(fixedPrice, req, res)
+        }
       }
     }
 
     return (req: express.Request, res: express.Response, next: express.NextFunction) => {
       log(`Requested ${req.path}`)
-      parseToken(req, (error, token) => {
-        return _guard(price, req, res, next, error, token)
+      parseToken(req, (error, token, meta) => {
+        return _guard(price!, req, res, next, error, token!, meta!)
       })
     }
   }
@@ -103,16 +111,24 @@ export default class Paywall {
     let handler: express.RequestHandler = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
       log('Called payment handler')
       try {
-        const body = await this.machinomy.acceptPayment(req.body)
+        const response = await fetcher.fetch(`${GATEWAY_URL}${PREFIX}/accept`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include',
+          body: JSON.stringify(req.body)
+        })
         log('Accept request')
-        res.status(202).header('Paywall-Token', body.token).send(body)
+        const json = await response.json()
+        res.status(202).header('Paywall-Token', json.token).send(json)
       } catch (e) {
         log('Reject request', e)
         next(e)
       }
     }
 
-    return function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    return (req: express.Request, res: express.Response, next: express.NextFunction) => {
       if (isAcceptUrl(req.url)) {
         handler(req, res, next)
       } else {
--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -1,13 +1,11 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
-import Machinomy from 'machinomy'
 import * as dotenv from 'dotenv'
 import HDWalletProvider from '@machinomy/hdwallet-provider'
-import * as Web3 from 'web3'
 import Paywall from './Paywall'
-import * as BigNumber from 'bignumber.js'
 import * as morgan from 'morgan'
 import * as url from 'url'
+import * as BigNumber from 'bignumber.js'
 
 async function main () {
   dotenv.config()
@@ -17,14 +15,12 @@ async function main () {
 
   const MNEMONIC = String(process.env.MNEMONIC).trim()
   const PROVIDER_URL = String(process.env.PROVIDER_URL)
-  const DATABASE_URL = String(process.env.DATABASE_URL)
+  const GATEWAY_URL = String(process.env.GATEWAY_URL)
 
   const provider = new HDWalletProvider(MNEMONIC, PROVIDER_URL)
-  const web3 = new Web3(provider)
   const account = await provider.getAddress(0)
-  const machinomy = new Machinomy(account, web3, { databaseUrl: DATABASE_URL })
-  const base = new url.URL(`https://${HOST}`)
-  const paywall = new Paywall(machinomy, account, base)
+  const base = new url.URL(GATEWAY_URL)
+  const paywall = new Paywall(account, base)
 
   let app = express()
   app.use(bodyParser.json())
