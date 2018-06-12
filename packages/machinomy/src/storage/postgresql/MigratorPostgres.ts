fa7fcce3fa400c025d2169319223c4efc1c8b104
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-12 16:26:55 +0300

    Replace dummy stuff with valuable in Postgres migrator.

--- packages/machinomy/src/storage/postgresql/MigratorPostgres.ts
+++ packages/machinomy/src/storage/postgresql/MigratorPostgres.ts
@@ -1,5 +1,9 @@
 import IMigrator from '../IMigrator'
 import EnginePostgres from './EnginePostgres'
+import * as fs from 'fs'
+const DBMigrate = require('db-migrate')
+const dbmigrate = DBMigrate.getInstance(true, { cwd: '../../../migrations', config: __dirname + '/../../../database.json' })
+const LENGTH_OF_MIGRATION_NAME = 14
 
 export default class MigratorPostgres implements IMigrator {
   engine: EnginePostgres
@@ -10,29 +14,74 @@ export default class MigratorPostgres implements IMigrator {
 
   isLatest (): Promise<boolean> {
     return new Promise(async (resolve) => {
-      resolve(true)
+      const commonIndex: number = await this.getCommonIndex()
+      if (commonIndex === -1) {
+        resolve(true)
+      } else {
+        resolve(false)
+      }
     })
   }
 
   sync (n?: string): Promise<void> {
     return new Promise(async (resolve) => {
+      if (n !== undefined) {
+        if (n.length === LENGTH_OF_MIGRATION_NAME) {
+          dbmigrate.sync(n)
+        } else {
+          console.error('DB migration name must have ' + LENGTH_OF_MIGRATION_NAME + ' chars. But got ' + n.length)
+        }
+      } else {
+        dbmigrate.sync()
+      }
       resolve()
     })
   }
 
   async getCommonIndex (): Promise<number> {
-    return Promise.resolve(-1)
+    const migrationsInDB = await this.retrieveUpMigrationList()
+    const migrationsInFolder = await this.retrieveInFolderMigrationList()
+    let commonIndex = -1
+    for (let i = 0; i < migrationsInFolder.length; i++) {
+      if (migrationsInDB[i] !== migrationsInFolder[i]) {
+        commonIndex = i
+        break
+      }
+    }
+
+    return Promise.resolve(commonIndex)
   }
 
   retrieveUpMigrationList (): Promise<string[]> {
     return new Promise((resolve) => {
-      resolve([])
+      // tslint:disable-next-line:no-floating-promises
+      this.engine.exec((client: any) => client.query(
+        'SELECT name FROM migrations ORDER BY name ASC'
+      )).then((res: any) => {
+        const names: string[] = res.rows
+        let result: string[] = []
+        for (let migrationName in names) {
+          result.push(migrationName.substring(1))
+        }
+        resolve(result)
+      })
     })
   }
 
   retrieveInFolderMigrationList (): Promise<string[]> {
     return new Promise(async (resolve) => {
-      resolve([])
+      let result: string[] = []
+      const listOfFiles: string[] = fs.readdirSync(__dirname + '/../../../migrations/')
+      console.log('debug::' + __dirname + '/../../../migrations/')
+      for (let filename of listOfFiles) {
+        const isDir = fs.statSync(__dirname + '/../../../migrations/' + filename).isDirectory()
+        if (!isDir) {
+          result.push(filename.slice(0, -3))
+        }
+      }
+      result.sort()
+      console.log('debug::DB migration files: ' + JSON.stringify(result))
+      resolve(result)
     })
   }
 }
