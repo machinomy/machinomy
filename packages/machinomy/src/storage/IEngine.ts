22dcf4e567cd79d689bb9d1dc57a992ada0392e8
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-12 16:04:20 +0300

    Add IMigrator. Add Migrator for SQLite.

--- packages/machinomy/src/storage/IEngine.ts
+++ packages/machinomy/src/storage/IEngine.ts
@@ -1,6 +1,9 @@
+import IMigrator from './IMigrator'
+
 export default interface IEngine {
   connect (): Promise<void>
   isConnected (): boolean
   close (): Promise<void>
   drop (): Promise<void>
+  migrate (): IMigrator
 }
