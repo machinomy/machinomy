ddcd8bccf0431169e6f0c5fff2d5f98b8a044b3b
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-12 16:17:49 +0300

    Add Migrators for Postgres, Nedb, Mongo

--- packages/machinomy/src/storage/nedb/EngineNedb.ts
+++ packages/machinomy/src/storage/nedb/EngineNedb.ts
@@ -1,12 +1,14 @@
 import * as Datastore from 'nedb'
 import IEngine from '../IEngine'
 import IExec from '../IExec'
+import MigratorNedb from './MigratorNedb'
 import NedbDatastore from './NedbDatastore'
 
 let db = new Map<string, NedbDatastore>()
 
 export default class EngineNedb implements IEngine, IExec<NedbDatastore> {
   datastore: NedbDatastore
+  migrator: MigratorNedb
 
   constructor (path: string, inMemoryOnly: boolean = false) {
     let found = db.get(path)
@@ -17,6 +19,7 @@ export default class EngineNedb implements IEngine, IExec<NedbDatastore> {
       this.datastore = new NedbDatastore(datastore)
       db.set(path, this.datastore)
     }
+    this.migrator = new MigratorNedb(this)
   }
 
   isConnected (): boolean {
@@ -39,5 +42,9 @@ export default class EngineNedb implements IEngine, IExec<NedbDatastore> {
 
   async exec <B> (fn: (client: NedbDatastore) => B): Promise<B> {
     return fn(this.datastore)
+  }
+
+  migrate (): MigratorNedb {
+    return this.migrator
   }
 }
