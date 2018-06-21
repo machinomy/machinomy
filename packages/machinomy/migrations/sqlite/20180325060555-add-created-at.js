d7bb1ff40c029c75e5b488f8c2a216a6a309d695
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-21 17:51:57 +0300

    Fix down() for removeColumn createdAt for SQLite migrations.

--- packages/machinomy/migrations/sqlite/20180325060555-add-created-at.js
+++ packages/machinomy/migrations/sqlite/20180325060555-add-created-at.js
@@ -20,8 +20,36 @@ exports.up = function(db) {
   });
 };
 
-exports.down = function(db) {
+exports.down = function(db, cb) {
+  removeColumn(db, 'payment', 'createdAt', cb)
 };
+
+function removeColumn(db, tableName, columnName, cb) {
+  db.all(`PRAGMA table_info(${tableName});`, async (err, rows) => {
+    const sqlNamesWithTypes = rows.filter((element) => {
+      return element.name !== columnName
+    }).map((element) => {
+      return `${element.name} ${element.type}`
+    }).join(', ')
+
+    const sqlNames = rows.filter((element) => {
+      return element.name !== columnName
+    }).map((element) => {
+      return `${element.name}`
+    }).join(', ')
+    const removeColumnSql =
+        `
+        CREATE TEMPORARY TABLE ${tableName + '_backup'}(${sqlNamesWithTypes});
+        INSERT INTO ${tableName + '_backup'} SELECT ${sqlNames} FROM ${tableName};
+        DROP TABLE ${tableName};
+        CREATE TABLE ${tableName}(${sqlNamesWithTypes});
+        INSERT INTO ${tableName} SELECT ${sqlNames} FROM ${tableName + '_backup'};
+        DROP TABLE ${tableName + '_backup'};
+       `
+    await db.runSql(removeColumnSql)
+    cb()
+  })
+}
 
 exports._meta = {
   "version": 1
