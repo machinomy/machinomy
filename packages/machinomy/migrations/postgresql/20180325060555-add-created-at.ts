5d8981c17cbf29517ce82c927055ca668270b725
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-22 14:43:20 +0300

    Change postgresql js-migrations to ts-migrations.

---
+++ packages/machinomy/migrations/postgresql/20180325060555-add-created-at.ts
@@ -0,0 +1,15 @@
+import { Base as DBMigrateBase, CallbackFunction } from 'db-migrate-base'
+
+let _meta: Object = {
+  version: 1
+}
+
+exports.up = (db: any, callback: CallbackFunction) => {
+  return db.addColumn('payment', 'createdAt', {
+    type: 'bigint'
+  })
+}
+
+exports.down = (db: DBMigrateBase, callback: CallbackFunction) => {
+  return db.removeColumn('payment', 'createdAt', callback)
+}
