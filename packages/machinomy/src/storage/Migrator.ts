76eb7e1a7bfca0504541279e162191318cdddb0d
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-23 18:10:22 +0300

    Variable dbmigrate become field of Migrator class.

--- packages/machinomy/src/storage/Migrator.ts
+++ packages/machinomy/src/storage/Migrator.ts
@@ -4,17 +4,17 @@ import * as fs from 'fs'
 import { ConnectionString } from 'connection-string'
 import * as DBMigrate from 'db-migrate'
 
-let dbmigrate: any
 const LENGTH_OF_MIGRATION_NAME = 14
 
 export default class Migrator implements IMigrator {
   engine: IEngine
   migrationsPath: string
+  dbmigrate: any
 
   constructor (engine: IEngine, connectionString: string, migrationsPath: string) {
     this.engine = engine
     const dbMigrateConfig = Migrator.generateConfigObject(connectionString)
-    dbmigrate = DBMigrate.getInstance(true, dbMigrateConfig)
+    this.dbmigrate = DBMigrate.getInstance(true, dbMigrateConfig)
     this.migrationsPath = migrationsPath
     if (this.migrationsPath.endsWith('/') !== true) {
       this.migrationsPath += '/'
@@ -67,18 +67,18 @@ export default class Migrator implements IMigrator {
 
   isLatest (): Promise<boolean> {
     return new Promise((resolve) => {
-      return resolve(dbmigrate.check())
+      return resolve(this.dbmigrate.check())
     })
   }
 
   sync (n?: string): Promise<void> {
     return new Promise(async (resolve) => {
       if (n !== undefined) {
-        dbmigrate.sync(n)
+        this.dbmigrate.sync(n)
       } else {
         const migrationsInFolder = await this.retrieveInFolderMigrationList()
         const lastMigrationInFolderName = migrationsInFolder[migrationsInFolder.length - 1].substring(0, LENGTH_OF_MIGRATION_NAME)
-        dbmigrate.sync(lastMigrationInFolderName)
+        this.dbmigrate.sync(lastMigrationInFolderName)
       }
       return resolve()
     })
