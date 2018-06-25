f5d5d4dac725ed8bb031080b4f1a8aaf515c541c
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-25 15:10:54 +0300

    Remove MongoDB.

--- packages/machinomy/src/MachinomyOptions.ts
+++ packages/machinomy/src/MachinomyOptions.ts
@@ -2,7 +2,7 @@ import * as BigNumber from 'bignumber.js'
 import { MigrateOption } from './MigrateOption'
 
 /**
- * Params for Machinomy. Currenty Machinomy supports mongodb and nedb as a database engine.
+ * Params for Machinomy. Currently Machinomy supports nedb and postgresql as a database engine.
  * Nedb is a default engine.
  */
 export default interface MachinomyOptions {
