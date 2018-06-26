d0855785cc4d17feb9feba08a5f69b7f3339b22d
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-26 17:11:45 +0300

    Add commands for Machinomy CLI.

---
+++ packages/cli/commands/channels.ts
@@ -0,0 +1,27 @@
+import * as configuration from '../../machinomy/lib/configuration'
+import Machinomy from '../../machinomy/lib/index'
+import * as Web3 from 'web3'
+import { PaymentChannelSerde } from '../../machinomy/lib/PaymentChannel'
+
+async function channels (): Promise<void> {
+  const settings = configuration.sender()
+  const provider = configuration.currentProvider()
+  const web3 = new Web3(provider)
+
+  if (!settings.account) {
+    return
+  }
+
+  const machinomy = new Machinomy(settings.account, web3, settings)
+
+  machinomy.channels().then((channels: any) => console.log(channels.map(PaymentChannelSerde.instance.serialize)))
+    .catch((e: any) => console.log(e))
+    .then(() => machinomy.shutdown())
+    .catch((e: any) => {
+      console.error('Failed to cleanly shut down:')
+      console.error(e)
+      process.exit(1)
+    })
+}
+
+export default channels
---
+++ packages/cli/commands/pry.ts
@@ -0,0 +1,27 @@
+import * as configuration from '../../machinomy/lib/configuration'
+import Web3 = require('web3')
+import Machinomy from '../../machinomy/lib/index'
+
+async function pry (uri: string) {
+  const settings = configuration.sender()
+  const provider = configuration.currentProvider()
+  const web3 = new Web3(provider)
+
+  if (!settings.account) {
+    return
+  }
+
+  const machinomy = new Machinomy(settings.account, web3, settings)
+
+  try {
+    let res = await machinomy.pry(uri)
+    console.log(res)
+    await machinomy.shutdown()
+  } catch (error) {
+    console.error('Failed to cleanly pry:')
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+export default pry
