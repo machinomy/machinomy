115276969314f4d42eb3992fd6a1bc7149842c5a
Andrei Riaskov <ryaskov.work@gmail.com>
2018-07-13 16:23:41 +0300

    Rewrite ChannelContract as router class.

--- packages/machinomy/src/ChannelContract.ts
+++ packages/machinomy/src/ChannelContract.ts
@@ -2,106 +2,102 @@ import * as Web3 from 'web3'
 import * as BigNumber from 'bignumber.js'
 import { TransactionResult } from 'truffle-contract'
 import Logger from '@machinomy/logger'
+import ChannelEthContract from './ChannelEthContract'
+import ChannelTokenContract from './ChannelTokenContract'
 import Signature from './Signature'
-import { Unidirectional } from '@machinomy/contracts'
 import ChannelId from './ChannelId'
 
 const LOG = new Logger('ChannelContract')
 
-const CREATE_CHANNEL_GAS = 300000
-
 export default class ChannelContract {
-  contract: Promise<Unidirectional.Contract>
+  channelEthContract: ChannelEthContract
+  channelTokenContract: ChannelTokenContract
 
   private web3: Web3
 
   constructor (web3: Web3) {
     this.web3 = web3
-    this.contract = Unidirectional.contract(this.web3.currentProvider).deployed()
+    this.channelEthContract = new ChannelEthContract(web3)
+    this.channelTokenContract = new ChannelTokenContract(web3)
   }
 
-  async open (sender: string, receiver: string, price: BigNumber.BigNumber, settlementPeriod: number | BigNumber.BigNumber, channelId?: ChannelId | string): Promise<TransactionResult> {
-    LOG.info(`Creating channel. Value: ${price} / Settlement: ${settlementPeriod}`)
-    let _channelId = channelId || ChannelId.random()
-    const deployed = await this.contract
-    return deployed.open(_channelId.toString(), receiver, new BigNumber.BigNumber(settlementPeriod), {
-      from: sender,
-      value: price,
-      gas: CREATE_CHANNEL_GAS
-    })
-  }
-
-  async claim (receiver: string, channelId: string, value: BigNumber.BigNumber, signature: Signature): Promise<TransactionResult> {
-    LOG.info(`Claiming channel with id ${channelId} on behalf of receiver ${receiver}`)
-    LOG.info(`Values: ${value} / Signature: ${signature.toString()}`)
-    const deployed = await this.contract
-    return deployed.claim(channelId, value, signature.toString(), { from: receiver })
-  }
-
-  async deposit (sender: string, channelId: string, value: BigNumber.BigNumber): Promise<TransactionResult> {
-    LOG.info(`Depositing ${value} into channel ${channelId}`)
-    const deployed = await this.contract
-    return deployed.deposit(channelId, {
-      from: sender,
-      value: value,
-      gas: CREATE_CHANNEL_GAS
-    })
-  }
-
-  async getState (channelId: string): Promise<number> {
-    LOG.info(`Fetching state for channel ${channelId}`)
-    const deployed = await this.contract
-    const isOpen = await deployed.isOpen(channelId)
-    const isSettling = await deployed.isSettling(channelId)
-
-    if (isOpen) {
-      return 0
+  async open (sender: string, receiver: string, price: BigNumber.BigNumber, settlementPeriod: number | BigNumber.BigNumber, channelId?: ChannelId | string, tokenContract?: string): Promise<TransactionResult> {
+    if (tokenContract) {
+      return this.channelTokenContract.open(sender, receiver, price, settlementPeriod, channelId)
+    } else {
+      return this.channelEthContract.open(sender, receiver, price, settlementPeriod, channelId)
     }
+  }
 
-    if (isSettling) {
-      return 1
+  async claim (receiver: string, channelId: string, value: BigNumber.BigNumber, signature: Signature, tokenContract?: string): Promise<TransactionResult> {
+    if (tokenContract) {
+      return this.channelTokenContract.claim(receiver, channelId, value, signature)
+    } else {
+      return this.channelEthContract.claim(receiver, channelId, value, signature)
     }
-
-    return 2
   }
 
-  async getSettlementPeriod (channelId: string): Promise<BigNumber.BigNumber> {
-    LOG.info(`Fetching settlement period for channel ${channelId}`)
-    const deployed = await this.contract
-    const exists = await deployed.isPresent(channelId)
-
-    if (!exists) {
-      throw new Error(`Cannot fetch settlement period for non-existent channel ${channelId}.`)
+  async deposit (sender: string, channelId: string, value: BigNumber.BigNumber, tokenContract?: string): Promise<TransactionResult> {
+    if (tokenContract) {
+      return this.channelTokenContract.deposit(sender, channelId, value)
+    } else {
+      return this.channelEthContract.deposit(sender, channelId, value)
     }
-
-    const chan = await deployed.channels(channelId)
-    return chan[3]
   }
 
-  async startSettle (account: string, channelId: string): Promise<TransactionResult> {
-    LOG.info(`Starting settle for account ${account} and channel id ${channelId}.`)
-    const deployed = await this.contract
-    return deployed.startSettling(channelId, { from: account })
+  async getState (channelId: string, tokenContract?: string): Promise<number> {
+    if (tokenContract) {
+      return this.channelTokenContract.getState(channelId)
+    } else {
+      return this.channelEthContract.getState(channelId)
+    }
   }
 
-  async finishSettle (account: string, channelId: string): Promise<TransactionResult> {
-    LOG.info(`Finishing settle for account ${account} and channel ID ${channelId}.`)
-    const deployed = await this.contract
-    return deployed.settle(channelId, { from: account, gas: 400000 })
+  async getSettlementPeriod (channelId: string, tokenContract?: string): Promise<BigNumber.BigNumber> {
+    if (tokenContract) {
+      return this.channelTokenContract.getSettlementPeriod(channelId)
+    } else {
+      return this.channelEthContract.getSettlementPeriod(channelId)
+    }
   }
 
-  async paymentDigest (channelId: string, value: BigNumber.BigNumber): Promise<string> {
-    const deployed = await this.contract
-    return deployed.paymentDigest(channelId, value)
+  async startSettle (account: string, channelId: string, tokenContract?: string): Promise<TransactionResult> {
+    if (tokenContract) {
+      return this.channelTokenContract.startSettle(account, channelId)
+    } else {
+      return this.channelEthContract.startSettle(account, channelId)
+    }
   }
 
-  async canClaim (channelId: string, payment: BigNumber.BigNumber, receiver: string, signature: Signature) {
-    const deployed = await this.contract
-    return deployed.canClaim(channelId, payment, receiver, signature.toString())
+  async finishSettle (account: string, channelId: string, tokenContract?: string): Promise<TransactionResult> {
+    if (tokenContract) {
+      return this.channelTokenContract.finishSettle(account, channelId)
+    } else {
+      return this.channelEthContract.finishSettle(account, channelId)
+    }
   }
 
-  async channelById (channelId: string): Promise<[string, string, BigNumber.BigNumber, BigNumber.BigNumber, BigNumber.BigNumber]> {
-    const deployed = await this.contract
-    return deployed.channels(channelId)
+  async paymentDigest (channelId: string, value: BigNumber.BigNumber, tokenContract?: string): Promise<string> {
+    if (tokenContract) {
+      return this.channelTokenContract.paymentDigest(channelId, value)
+    } else {
+      return this.channelEthContract.paymentDigest(channelId, value)
+    }
+  }
+
+  async canClaim (channelId: string, payment: BigNumber.BigNumber, receiver: string, signature: Signature, tokenContract?: string) {
+    if (tokenContract) {
+      return this.channelTokenContract.canClaim(channelId, payment, receiver, signature)
+    } else {
+      return this.channelEthContract.canClaim(channelId, payment, receiver, signature)
+    }
+  }
+
+  async channelById (channelId: string, tokenContract?: string): Promise<[string, string, BigNumber.BigNumber, BigNumber.BigNumber, BigNumber.BigNumber]> {
+    if (tokenContract) {
+      return this.channelTokenContract.channelById(channelId)
+    } else {
+      return this.channelEthContract.channelById(channelId)
+    }
   }
 }
