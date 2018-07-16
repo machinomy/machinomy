35d77e4853352c65023edb112142d19673c0fe8c
Andrei Riaskov <ryaskov.work@gmail.com>
2018-07-16 17:54:59 +0300

    Back to this.contract instead of this.unidirectionalContract.

--- packages/machinomy/src/ChannelTokenContract.ts
+++ packages/machinomy/src/ChannelTokenContract.ts
@@ -12,20 +12,20 @@ const CREATE_CHANNEL_GAS = new BigNumber.BigNumber(300000)
 
 // FIXME MOVE TOKENS SOMEWHERE HERE
 export default class ChannelTokenContract {
-  unidirectionalContract: Promise<contracts.TokenUnidirectional.Contract>
+  contract: Promise<contracts.TokenUnidirectional.Contract>
 
   private web3: Web3
 
   constructor (web3: Web3) {
     this.web3 = web3
-    this.unidirectionalContract = contracts.TokenUnidirectional.contract(this.web3.currentProvider).deployed()
+    this.contract = contracts.TokenUnidirectional.contract(this.web3.currentProvider).deployed()
   }
 
   async open (sender: string, receiver: string, price: BigNumber.BigNumber, settlementPeriod: number | BigNumber.BigNumber, tokenContract: string, channelId?: ChannelId | string): Promise<TransactionResult> {
     LOG.info(`Creating channel. Value: ${price} / Settlement: ${settlementPeriod}`)
     let _channelId = channelId || ChannelId.random()
     const standardTokenContract = contracts.StandardToken.contract(this.web3.currentProvider).at(tokenContract)
-    const deployedTokenUnidirectional = await this.unidirectionalContract
+    const deployedTokenUnidirectional = await this.contract
     const deployedStandardTokenContract = await standardTokenContract
 
     const approveTx = await deployedStandardTokenContract.approve(receiver, price, { from: sender })
@@ -45,14 +45,14 @@ export default class ChannelTokenContract {
   async claim (receiver: string, channelId: string, value: BigNumber.BigNumber, signature: Signature): Promise<TransactionResult> {
     LOG.info(`Claiming channel with id ${channelId} on behalf of receiver ${receiver}`)
     LOG.info(`Values: ${value} / Signature: ${signature.toString()}`)
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     return deployed.claim(channelId, value, signature.toString(), { from: receiver })
   }
 
   async deposit (sender: string, channelId: string, value: BigNumber.BigNumber, tokenContract: string): Promise<TransactionResult> {
     LOG.info(`Depositing ${value} into channel ${channelId}`)
     const standardTokenContract = contracts.StandardToken.contract(this.web3.currentProvider).at(tokenContract)
-    const deployedTokenUnidirectional = await this.unidirectionalContract
+    const deployedTokenUnidirectional = await this.contract
     const deployedStandardTokenContract = await standardTokenContract
     const channel = await this.channelById(channelId)
     const receiver = channel[1]
@@ -72,7 +72,7 @@ export default class ChannelTokenContract {
 
   async getState (channelId: string): Promise<number> {
     LOG.info(`Fetching state for channel ${channelId}`)
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     const isOpen = await deployed.isOpen(channelId)
     const isSettling = await deployed.isSettling(channelId)
 
@@ -89,7 +89,7 @@ export default class ChannelTokenContract {
 
   async getSettlementPeriod (channelId: string): Promise<BigNumber.BigNumber> {
     LOG.info(`Fetching settlement period for channel ${channelId}`)
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     const exists = await deployed.isPresent(channelId)
 
     if (!exists) {
@@ -102,28 +102,28 @@ export default class ChannelTokenContract {
 
   async startSettle (account: string, channelId: string): Promise<TransactionResult> {
     LOG.info(`Starting settle for account ${account} and channel id ${channelId}.`)
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     return deployed.startSettling(channelId, { from: account })
   }
 
   async finishSettle (account: string, channelId: string): Promise<TransactionResult> {
     LOG.info(`Finishing settle for account ${account} and channel ID ${channelId}.`)
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     return deployed.settle(channelId, { from: account, gas: 400000 })
   }
 
   async paymentDigest (channelId: string, value: BigNumber.BigNumber, tokenContract: string): Promise<string> {
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     return deployed.paymentDigest(channelId, value, tokenContract)
   }
 
   async canClaim (channelId: string, payment: BigNumber.BigNumber, receiver: string, signature: Signature): Promise<boolean> {
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     return deployed.canClaim(channelId, payment, receiver, signature.toString())
   }
 
   async channelById (channelId: string): Promise<[string, string, BigNumber.BigNumber, BigNumber.BigNumber, BigNumber.BigNumber, string]> {
-    const deployed = await this.unidirectionalContract
+    const deployed = await this.contract
     return deployed.channels(channelId)
   }
 }
