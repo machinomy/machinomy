f1c47cd43c0df8029d14255ecb97fc8854750564
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-07-23 15:41:10 +0300

    Deploy contracts to Ropsten

--- packages/contracts/migrations/3_deploy_token_unidirectional.ts
+++ packages/contracts/migrations/3_deploy_token_unidirectional.ts
@@ -4,9 +4,7 @@ const ECRecovery = artifacts.require('./ECRecovery.sol')
 const TokenUnidirectional = artifacts.require('./TokenUnidirectional.sol')
 
 module.exports = function (deployer: Deployer) {
-  return deployer.deploy(ECRecovery).then(() => {
-    return deployer.link(ECRecovery, TokenUnidirectional)
-  }).then(() => {
+  return deployer.link(ECRecovery, TokenUnidirectional).then(() => {
     return deployer.deploy(TokenUnidirectional)
   })
 }
