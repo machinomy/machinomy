6146181cb0041be986d795612134468fffd4f960
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-07-30 16:59:53 +0300

    WIP

--- packages/machinomy/src/Storage.ts
+++ packages/machinomy/src/Storage.ts
@@ -15,6 +15,55 @@ export interface Storage {
   migrator: IMigrator | undefined
 }
 
+async function buildNedb (databaseUrl: string, inflator: ChannelInflator, namespace: string): Promise<Storage> {
+  let EngineNedb = (await import('./storage/nedb/EngineNedb')).default
+  let NedbTokensDatabase = (await import('./storage/nedb/NedbTokensDatabase')).default
+  let NedbPaymentsDatabase = (await import('./storage/nedb/NedbPaymentsDatabase')).default
+  let NedbChannelsDatabase = (await import('./storage/nedb/NedbChannelsDatabase')).default
+
+  let engine = new EngineNedb(databaseUrl, false)
+  return {
+    engine: engine,
+    tokensDatabase: new NedbTokensDatabase(engine, namespace),
+    paymentsDatabase: new NedbPaymentsDatabase(engine, namespace),
+    channelsDatabase: new NedbChannelsDatabase(engine, inflator, namespace),
+    migrator: undefined
+  }
+}
+
+async function buildSqlite (databaseUrl: string, inflator: ChannelInflator, namespace: string): Promise<Storage> {
+  let EngineSqlite = (await import('./storage/sqlite/EngineSqlite')).default
+  let SqliteTokensDatabase = (await import('./storage/sqlite/SqliteTokensDatabase')).default
+  let SqlitePaymentsDatabase = (await import('./storage/sqlite/SqlitePaymentsDatabase')).default
+  let SqliteChannelsDatabase = (await import('./storage/sqlite/SqliteChannelsDatabase')).default
+
+  let engine = new EngineSqlite(databaseUrl)
+  return {
+    engine: engine,
+    tokensDatabase: new SqliteTokensDatabase(engine, namespace),
+    paymentsDatabase: new SqlitePaymentsDatabase(engine, namespace),
+    channelsDatabase: new SqliteChannelsDatabase(engine, inflator, namespace),
+    migrator: new Migrator(engine, databaseUrl, resolvePath('migrations/sqlite/'))
+  }
+}
+
+async function buildPostgres (databaseUrl: string, inflator: ChannelInflator, namespace: string): Promise<Storage> {
+  let EnginePostgres = (await import('./storage/postgresql/EnginePostgres')).default
+  let PostgresTokensDatabase = (await import('./storage/postgresql/PostgresTokensDatabase')).default
+  let PostgresPaymentsDatabase = (await import('./storage/postgresql/PostgresPaymentsDatabase')).default
+  let PostgresChannelsDatabase = (await import('./storage/postgresql/PostgresChannelsDatabase')).default
+
+  let engine = new EnginePostgres(databaseUrl)
+
+  return {
+    engine: engine,
+    tokensDatabase: new PostgresTokensDatabase(engine, namespace),
+    paymentsDatabase: new PostgresPaymentsDatabase(engine, namespace),
+    channelsDatabase: new PostgresChannelsDatabase(engine, inflator, namespace),
+    migrator: new Migrator(engine, databaseUrl, resolvePath('migrations/postgresql/'))
+  }
+}
+
 export namespace Storage {
   export function build (databaseUrl: string, inflator: ChannelInflator): Promise<Storage> {
     const splits = databaseUrl.split('://')
@@ -30,55 +79,6 @@ export namespace Storage {
         return buildSqlite(databaseUrl, inflator, namespace)
       default:
         throw new Error(`Unsupported database protocol: ${protocol}`)
-    }
-  }
-
-  async function buildNedb (databaseUrl: string, inflator: ChannelInflator, namespace: string): Promise<Storage> {
-    let EngineNedb = (await import('./storage/nedb/EngineNedb')).default
-    let NedbTokensDatabase = (await import('./storage/nedb/NedbTokensDatabase')).default
-    let NedbPaymentsDatabase = (await import('./storage/nedb/NedbPaymentsDatabase')).default
-    let NedbChannelsDatabase = (await import('./storage/nedb/NedbChannelsDatabase')).default
-
-    let engine = new EngineNedb(databaseUrl, false)
-    return {
-      engine: engine,
-      tokensDatabase: new NedbTokensDatabase(engine, namespace),
-      paymentsDatabase: new NedbPaymentsDatabase(engine, namespace),
-      channelsDatabase: new NedbChannelsDatabase(engine, inflator, namespace),
-      migrator: undefined
-    }
-  }
-
-  async function buildSqlite (databaseUrl: string, inflator: ChannelInflator, namespace: string): Promise<Storage> {
-    let EngineSqlite = (await import('./storage/sqlite/EngineSqlite')).default
-    let SqliteTokensDatabase = (await import('./storage/sqlite/SqliteTokensDatabase')).default
-    let SqlitePaymentsDatabase = (await import('./storage/sqlite/SqlitePaymentsDatabase')).default
-    let SqliteChannelsDatabase = (await import('./storage/sqlite/SqliteChannelsDatabase')).default
-
-    let engine = new EngineSqlite(databaseUrl)
-    return {
-      engine: engine,
-      tokensDatabase: new SqliteTokensDatabase(engine, namespace),
-      paymentsDatabase: new SqlitePaymentsDatabase(engine, namespace),
-      channelsDatabase: new SqliteChannelsDatabase(engine, inflator, namespace),
-      migrator: new Migrator(engine, databaseUrl, resolvePath('migrations/sqlite/'))
-    }
-  }
-
-  async function buildPostgres (databaseUrl: string, inflator: ChannelInflator, namespace: string): Promise<Storage> {
-    let EnginePostgres = (await import('./storage/postgresql/EnginePostgres')).default
-    let PostgresTokensDatabase = (await import('./storage/postgresql/PostgresTokensDatabase')).default
-    let PostgresPaymentsDatabase = (await import('./storage/postgresql/PostgresPaymentsDatabase')).default
-    let PostgresChannelsDatabase = (await import('./storage/postgresql/PostgresChannelsDatabase')).default
-
-    let engine = new EnginePostgres(databaseUrl)
-
-    return {
-      engine: engine,
-      tokensDatabase: new PostgresTokensDatabase(engine, namespace),
-      paymentsDatabase: new PostgresPaymentsDatabase(engine, namespace),
-      channelsDatabase: new PostgresChannelsDatabase(engine, inflator, namespace),
-      migrator: new Migrator(engine, databaseUrl, resolvePath('migrations/postgresql/'))
     }
   }
 }
