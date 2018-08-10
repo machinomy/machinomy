fd76278589cbd89597afe75430fa40c8f5333e27
Andrei Riaskov <ryaskov.work@gmail.com>
2018-08-10 17:48:24 +0300

    Add meta information to logdna's logger calls.

--- packages/logger/src/Logger.ts
+++ packages/logger/src/Logger.ts
@@ -26,7 +26,7 @@ export default class Logger {
         logDNAOptions = {}
       }
       if (!logDNAOptions.hasOwnProperty('hostname')) {
-        logDNAOptions = { ...logDNAOptions, hostname: 'defaultHostname' }
+        logDNAOptions = { ...logDNAOptions, hostname: '' }
       }
       this.loggerDNA = LogDNA.setupDefaultLogger(logDNAKey, logDNAOptions)
     }
@@ -35,42 +35,72 @@ export default class Logger {
   fatal (formatter: any, ...args: any[]) {
     this._fatal(formatter, args)
     if (this.loggerDNA) {
-      this.loggerDNA.fatal(`fatal:${this.namespace} ${formatter}`)
+      this.loggerDNA.log(formatter, {
+        level: 'Fatal',
+        app: '',
+        meta: { namespace: this.namespace },
+        index_meta: true
+      })
     }
   }
 
   error (formatter: any, ...args: any[]) {
     this._error(formatter, args)
     if (this.loggerDNA) {
-      this.loggerDNA.error(`error:${this.namespace} ${formatter}`)
+      this.loggerDNA.log(formatter, {
+        level: 'Error',
+        app: '',
+        meta: { namespace: this.namespace },
+        index_meta: true
+      })
     }
   }
 
   warn (formatter: any, ...args: any[]) {
     this._warn(formatter, args)
     if (this.loggerDNA) {
-      this.loggerDNA.warn(`warn:${this.namespace} ${formatter}`)
+      this.loggerDNA.log(formatter, {
+        level: 'Warn',
+        app: '',
+        meta: { namespace: this.namespace },
+        index_meta: true
+      })
     }
   }
 
   info (formatter: any, ...args: any[]) {
     this._info(formatter, args)
     if (this.loggerDNA) {
-      this.loggerDNA.info(`info:${this.namespace} ${formatter}`)
+      this.loggerDNA.log(formatter, {
+        level: 'Info',
+        app: '',
+        meta: { namespace: this.namespace },
+        index_meta: true
+      })
     }
   }
 
   debug (formatter: any, ...args: any[]) {
     this._debug(formatter, args)
     if (this.loggerDNA) {
-      this.loggerDNA.debug(`debug:${this.namespace} ${formatter}`)
+      this.loggerDNA.log(formatter, {
+        level: 'Debug',
+        app: '',
+        meta: { namespace: this.namespace },
+        index_meta: true
+      })
     }
   }
 
   trace (formatter: any, ...args: any[]) {
     this._trace(formatter, args)
     if (this.loggerDNA) {
-      this.loggerDNA.trace(`trace:${this.namespace} ${formatter}`)
+      this.loggerDNA.log(formatter, {
+        level: 'Trace',
+        app: '',
+        meta: { namespace: this.namespace },
+        index_meta: true
+      })
     }
   }
 }
