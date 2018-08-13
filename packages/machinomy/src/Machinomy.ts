af4dda771dbc56b44bbd17127b6058cbeda42a79
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-08-13 13:54:42 +0300

    Machinomy.registry is now public

--- packages/machinomy/src/Machinomy.ts
+++ packages/machinomy/src/Machinomy.ts
@@ -29,9 +29,10 @@ import { memoize } from 'decko'
  * [deposit]{@link Machinomy.deposit} methods.
  */
 export default class Machinomy {
+  readonly registry: Registry
+
   /** Ethereum account address that sends the money. */
   private readonly account: string
-  private readonly registry: Registry
 
   constructor (account: string, web3: Web3, options?: MachinomyOptions) {
     this.account = account
